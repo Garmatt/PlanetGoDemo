c09178c6f1590e3f653ad123cbb3c173afe3bcda
Garmatt <mattia.garassini@gmail.com>
2017-10-27 01:21:47 +0200

    Update board.js

--- PlanetGo_GameScriptDemo_files/board.js
+++ PlanetGo_GameScriptDemo_files/board.js
@@ -16,62 +16,57 @@
 
         },
         controller: ['$scope', function BoardController($scope) {
-
             $scope.MakeMove = function (x, y) {
+                console.log('-------------');
                 var selectedPoint = $scope.Board.GetPoint(x, y);
-                if (selectedPoint.Group != null)
+                if (selectedPoint.Group) {
                     return;
-
+                }
                 var color = $scope.Board.NextToPlay;
+                console.log('Playing ' + selectedPoint + ' ' + color);
                 var neighbors = selectedPoint.GetNeighbors();
-
-                var neighboringGroupsOfOppositeColor = [];
-                for (var n = 0; n < neighbors.length; n++) {
-                    var neighbor = neighbors[n];
-                    if (neighbor.Group && neighbor.Group.Color != color)
-                        neighboringGroupsOfOppositeColor.push(neighbor.Group);
-                }
-                neighboringGroupsOfOppositeColor = neighboringGroupsOfOppositeColor.unique();
-
-                var isMoveLegal = false;
-                for (var d = 0; d < neighboringGroupsOfOppositeColor.length; d++) {
-                    if (neighboringGroupsOfOppositeColor[d].GetLiberties() <= 1)
-                        isMoveLegal = true;
-                }
-
-                var neighboringGroupsOfSameColor = [];
-                for (var n = 0; n < neighbors.length; n++) {
-                    var neighbor = neighbors[n];
-                    if (neighbor.Group && neighbor.Group.Color == color)
-                        neighboringGroupsOfSameColor.push(neighbor.Group);
-                }
-                neighboringGroupsOfSameColor = neighboringGroupsOfSameColor.unique();
-
+                var neighboringGroupsOfOppositeColor = neighbors.filter(function (neighbor) {
+                    return neighbor.Group && neighbor.Group.Color !== color;
+                }).map(function (neighbor) {
+                    return neighbor.Group;
+                    }).unique();
+                console.log('Neighboring groups of opposite color: ' + neighboringGroupsOfOppositeColor.join('; '));
+                var isMoveLegal = neighboringGroupsOfOppositeColor.some(function (group) { return group.GetLiberties() <= 1; });
                 var newGroup = new Group(color);
-                newGroup.AddStone(selectedPoint);
-
+                newGroup.AddStone(selectedPoint, true);
+                console.log('New group: ' + newGroup);
+                var neighboringGroupsOfSameColor = neighbors.filter(function (neighbor) {
+                    return neighbor.Group && neighbor.Group.Color === color;
+                }).map(function (neighbor) {
+                    return neighbor.Group;
+                }).unique();
+                console.log('Neighboring groups of same color: ' + neighboringGroupsOfSameColor.join('; '));
                 neighboringGroupsOfSameColor.push(newGroup);
-                newGroup = $scope.Board.GetConnectedGroup(neighboringGroupsOfSameColor);
-                isMoveLegal = isMoveLegal || (newGroup.GetLiberties() > 0);
-
-                if (!isMoveLegal)
+                var connectedGroup = $scope.Board.GetConnectedGroup(neighboringGroupsOfSameColor);
+                console.log('Connected group: ' + connectedGroup);
+                isMoveLegal = isMoveLegal || (connectedGroup.GetLiberties() > 0);
+                if (!isMoveLegal) {
+                    selectedPoint.Group = null;
                     return;
-
-                $scope.Board.AddGroup(newGroup);
-                for (var r = 0; r < neighboringGroupsOfOppositeColor.length; r++) {
-                    var groupToCheckRemove = neighboringGroupsOfOppositeColor[r];
-                    if (groupToCheckRemove.GetLiberties() < 1)
+                }
+                neighboringGroupsOfSameColor.forEach(function (groupToRemove) {
+                    $scope.Board.RemoveGroup(groupToRemove);
+                });
+                $scope.Board.AddGroup(connectedGroup);
+                console.log('Added group: ' + connectedGroup);
+                neighboringGroupsOfOppositeColor.forEach(function (groupToCheckRemove) {
+                    if (groupToCheckRemove.GetLiberties() < 1) {
                         $scope.Board.RemoveGroup(groupToCheckRemove);
-                };
-
-                if (color == 'black') {
+                    }
+                });
+                if (color === 'black') {
                     $scope.Board.NextToPlay = 'white';
                 }
                 else {
                     $scope.Board.NextToPlay = 'black';
                 }
+                console.log(' ');
             };
-
         }],
         //templateUrl: '/templates/board.html'
         template: "<table class='goban playable' cellspacing='0'><tbody><tr ng-repeat='y in GetIndices() track by $index'><td ng-repeat='x in GetIndices() track by $index'><board-point point='Board.GetPoint($index, size - 1 - $parent.$index)' ng-click='MakeMove($index, size - 1 - $parent.$index)'></board-point></td></tr></tbody></table>"
