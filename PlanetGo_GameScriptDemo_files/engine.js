66db757dd8426d6fb89175d2658f3c2a606477c7
Garmatt <mattia.garassini@gmail.com>
2017-10-27 01:19:54 +0200

    Update engine.js

--- PlanetGo_GameScriptDemo_files/engine.js
+++ PlanetGo_GameScriptDemo_files/engine.js
@@ -1,173 +1,143 @@
-﻿function Point(x, y, board) {
-    var thisPoint = this;
-    thisPoint.Board = board;
-    thisPoint.X = x;
-    thisPoint.Y = y;
-    thisPoint.Group = null;
-    thisPoint.NeighborN = null;
-    thisPoint.NeighborE = null;
-    thisPoint.NeighborS = null;
-    thisPoint.NeighborW = null;
-    thisPoint.GetNeighbors = function () {
-        let neighbors = [];
-        if (thisPoint.NeighborN != null)
-            neighbors.push(thisPoint.NeighborN);
-        if (thisPoint.NeighborE != null)
-            neighbors.push(thisPoint.NeighborE);
-        if (thisPoint.NeighborS != null)
-            neighbors.push(thisPoint.NeighborS);
-        if (thisPoint.NeighborW != null)
-            neighbors.push(thisPoint.NeighborW);
-        return neighbors;
+function Point(x, y, board) {
+    if (!(this instanceof Point)) {
+        return new Point(x, y, board);
     }
+    this.Board = board;
+    this.X = x;
+    this.Y = y;
+    this.Group = null;
+    this.NeighborN = null;
+    this.NeighborE = null;
+    this.NeighborS = null;
+    this.NeighborW = null;
 };
+
+Point.prototype.GetNeighbors = function () {
+    return [this.NeighborN, this.NeighborE, this.NeighborS, this.NeighborW].filter(function (val) { return val !== null; });
+}
+
+Point.prototype.Equals = function (otherPoint) {
+    return this.X === otherPoint.X && this.Y === otherPoint.Y;
+}
+
+Point.prototype.toString = function () {
+    return "(" + this.X + "," + this.Y + ")";
+}
 
 function Group(color) {
-    var thisGroup = this;
-    thisGroup.Color = color;
-    thisGroup.Stones = [];
-    thisGroup.NeighboringPoints = [];
-	thisGroup.Contains = function (point) {
-		for (let s = 0; s < thisGroup.Stones.length; s++)
-        {
-            let stone = thisGroup.Stones[s];
-            if (stone.X == point.X && stone.Y == point.Y)
-                return true;
-        }
-	};
-	thisGroup.AddStone = function (point) {
-        if (thisGroup.Contains(point))
-			return false;
-			
-        thisGroup.Stones.push(point);
-        var neighbors = point.GetNeighbors();
-        var neighborsToAdd = [];
-        for (let n = 0; n < neighbors.length; n++)
-        {
-            let neighbor = neighbors[n];
-            let alreadyThere = thisGroup.Contains(neighbor);
-			if (alreadyThere)
-				continue;
-				
-            for (let l = 0; l < thisGroup.NeighboringPoints.length; l++)            
-            {
-                let liberty = thisGroup.NeighboringPoints[l];
-                if (neighbor.X == liberty.X && neighbor.Y == liberty.Y)
-                    alreadyThere = true;
-            }
-            if (alreadyThere)
-				continue;
-				
-			neighborsToAdd.push(neighbor);
-        }
-		
-		var indexToRemove = null;
-		for (let l = 0; l < thisGroup.NeighboringPoints.length; l++)  
-		{
-			let liberty = thisGroup.NeighboringPoints[l];
-            if (point.X == liberty.X && point.Y == liberty.Y){
-				indexToRemove = l;
-				break;
-			}			               
-        }		
-		
-        for (let a = 0; a < neighborsToAdd.length; a++)
-            thisGroup.NeighboringPoints.push(neighborsToAdd[a]);
-		
-		if (indexToRemove)
-			thisGroup.NeighboringPoints.splice(indexToRemove,1);
-
-        return true;
+    if (!(this instanceof Group)) {
+        return new Group(color);
     }
-    thisGroup.GetLiberties = function () {
-        let liberties = 0;
-        for (var n = 0; n < thisGroup.NeighboringPoints.length; n++)
-        {
-            let point = thisGroup.NeighboringPoints[n];
-            if (point.Group == null)
-                liberties = liberties + 1;
+    this.Color = color;
+    this.Stones = [];
+    this.NeighboringPoints = [];
+    this.Board = null;
+};
+
+Group.prototype.toString = function () {
+    return this.Color + ' group with ' + this.GetLiberties() + ' liberties: ' + this.Stones.join('-');
+}
+
+Group.prototype.AddStone = function (point, assignToGroup) {
+    var compareToPoint = function (p) {
+        return function (val) {
+            return val.X === p.X && val.Y === p.Y;
+        };
+    };
+    if (this.Stones.some(compareToPoint(point))) {
+        return false;
+    }
+    this.Stones.push(point);
+    if (assignToGroup) {
+        point.Group = this;
+    }
+    var neighborToRemove = this.NeighboringPoints.find(compareToPoint(point));
+    if (neighborToRemove) {
+        this.NeighboringPoints.splice(this.NeighboringPoints.indexOf(neighborToRemove), 1);
+    }
+    var neighborsToAdd = point.GetNeighbors().filter(function (val) {
+        return !(this.Stones.some(compareToPoint(val))) || (this.NeighboringPoints.some(compareToPoint(val)));
+    }, this);
+    if (neighborsToAdd.length > 0) {
+        this.NeighboringPoints = this.NeighboringPoints.concat(neighborsToAdd);
+    }
+    return true;
+}
+
+Group.prototype.GetLiberties = function () {
+    return this.NeighboringPoints.filter(function (point) { return point.Group === null; }).length || 0;
+}
+
+function Board(size) {
+    if (!(this instanceof Board)) {
+        return new Board(size);
+    }
+    this.Size = size;
+    this.IsSizeOdd = (size % 2) == 1;
+    this.IsSizeLarge = size >= 13;
+    this.Groups = [];
+    this.Points = new Array(size);
+    for (let i = 0; i < size; i++) {
+        let line = new Array(size);
+        let previousLine = this.Points[i - 1];
+        for (let j = 0; j < size; j++) {
+            let currentPoint = new Point(i + 1, j + 1, this);
+            if (j > 0) {
+                let neighborW = line[j - 1];
+                neighborW.NeighborE = currentPoint;
+                currentPoint.NeighborW = neighborW;
+            };
+            if (i > 0) {
+                let neighborN = previousLine[j];
+                neighborN.NeighborS = currentPoint;
+                currentPoint.NeighborN = neighborN;
+            };
+            line[j] = currentPoint;
         }
-        return liberties;
+        this.Points[i] = line;
     }
 };
 
-function Board(size) {
+Board.prototype.GetPoint = function (x, y) {
+    return this.Points[x][y];
+}
 
-    var thisBoard = this;
+Board.prototype.AddGroup = function (group) {
+    this.Groups.push(group);
+    group.Stones.forEach(function (stone) {
+        stone.Group = group;
+    });
+    group.Board = this;
+};
 
-    thisBoard.Size = size;
-
-    thisBoard.IsSizeOdd = (size % 2) == 1;
-
-    thisBoard.IsSizeLarge = size >= 13;
-
-    var points = new Array(size);
-    for (var i = 0; i < size; i++) {
-        var line = new Array(size);
-        var previousLine = points[i - 1];
-        for (var j = 0; j < size; j++) {
-            var thisPoint = new Point(i + 1, j + 1, thisBoard);
-            if (j > 0) {
-                var neighborW = line[j - 1];
-                neighborW.NeighborE = thisPoint;
-                thisPoint.NeighborW = neighborW;
-            };
-            if (i > 0) {
-                var neighborN = previousLine[j];
-                neighborN.NeighborS = thisPoint;
-                thisPoint.NeighborN = neighborN;
-            };
-            line[j] = thisPoint;
-        }
-        points[i] = line;
+Board.prototype.RemoveGroup = function (group) {
+    var index = this.Groups.indexOf(group);
+    if (index > -1) {
+        this.Groups.splice(index, 1);
+        group.Board = null;
+        group.Stones.forEach(function (stone) {
+            stone.Group = null;
+        });
     }
+}
 
-    thisBoard.Points = points;
-
-    thisBoard.GetPoint = function (x, y) {
-        return thisBoard.Points[x][y];
-    }
-
-    thisBoard.Groups = [];
-
-    thisBoard.AddGroup = function (group) {
-        thisBoard.Groups.push(group);
-        for (var s = 0; s < group.Stones.length; s++) {
-            let stone = group.Stones[s];
-            stone.Group = group;
-        };
-    };
-
-    thisBoard.RemoveGroup = function (group) {
-        var index = thisBoard.Groups.indexOf(group);
-        if (index > -1) {
-            thisBoard.Groups.splice(index, 1);
-            for (var s = 0; s < group.Stones.length; s++) {
-                let stone = group.Stones[s];
-                stone.Group = null;
-            };
-        }
-    }
-
-    thisBoard.GetConnectedGroup = function (groups) {
-        var stones = [];
-        for (var g = 0; g < groups.length; g++) {
-            var group = groups[g];
-            for (var s = 0; s < group.Stones.length; s++)
-                stones.push(group.Stones[s]);
-        };
-        stones = stones.unique();
-        var connectedGroup = new Group(groups[0].Color);
-        for (var i = 0; i < stones.length; i++)
-            connectedGroup.AddStone(stones[i]);
-
-        return connectedGroup;
-    };
+Board.prototype.GetConnectedGroup = function (groups) {
+    var stones = [];
+    groups.forEach(function (group) {
+        stones = stones.concat(group.Stones);
+    });
+    stones = stones.unique();
+    var connectedGroup = new Group(groups[0].Color);
+    stones.forEach(function (stone) {
+        connectedGroup.AddStone(stone, false);
+    });
+    return connectedGroup;
 };
 
 Array.prototype.contains = function (v) {
     for (var i = 0; i < this.length; i++) {
-        if (this[i] === v) return true;
+        if (this[i] === v)
+            return true;
     }
     return false;
 };
